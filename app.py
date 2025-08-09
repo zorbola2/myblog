73c9859e3bda3806d2866299cb20f548f0e63060
abdulloh <raxmonovzokir138@gmail.com>
2025-08-09 16:20:49 +0500

    first commit

--- app.py
+++ app.py
@@ -1,44 +1,94 @@
-from flask import Flask, render_template, request, redirect, url_for
+# app.py
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, Post, User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-posts = []
-post_id = 1
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
+    posts = Post.query.order_by(Post.id.desc()).all()
     return render_template('index.html', posts=posts)
 
-@app.route('/post/<int:id>')
-def post_detail(id):
-    post = next((p for p in posts if p['id'] == id), None)
-    return render_template('post_detail.html', post=post)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed = generate_password_hash(password)
+        user = User(username=username, password=hashed)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Now login.")
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
-    global post_id
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        posts.append({'id': post_id, 'title': title, 'content': content})
-        post_id += 1
+        post = Post(title=title, content=content)
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('create.html')
 
+@app.route('/post/<int:id>')
+def post_detail(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post_detail.html', post=post)
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
-    post = next((p for p in posts if p['id'] == id), None)
+    post = Post.query.get_or_404(id)
     if request.method == 'POST':
-        post['title'] = request.form['title']
-        post['content'] = request.form['content']
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('update.html', post=post)
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
-    global posts
-    posts = [p for p in posts if p['id'] != id]
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ models.py
@@ -0,0 +1,15 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
